1a786b69c3bb389b340ada455dc5859803229627
teria <>
2022-02-12 18:53:32 +0900

    改行文字込みで変数内容を画面上にリアルタイム表示成功

--- js/plugins.js
+++ js/plugins.js
@@ -14,5 +14,8 @@ var $plugins =
 {"name":"EventInformation","status":false,"description":"イベントの頭上に文字を表示したい時に使います","parameters":{}},
 {"name":"websocket","status":false,"description":"イベントの頭上に文字を表示したい時に使います","parameters":{}},
 {"name":"CommonInterceptor","status":true,"description":"割り込みコモンイベントプラグイン","parameters":{"ニューゲームコモン":"11","ロードコモン":"12","メニューコモン":"0"}},
-{"name":"TMNamePop2","status":true,"description":"イベントの頭上に文字列を表示する機能を追加します。","parameters":{"backOpacity":"96","fontSize":"20","outlineWidth":"4","outlineColor":"rgba(0, 0, 0, 0.5)","width":"160","roundRectRadius":"6"}}
+{"name":"TMNamePop2","status":true,"description":"イベントの頭上に文字列を表示する機能を追加します。","parameters":{"backOpacity":"96","fontSize":"20","outlineWidth":"4","outlineColor":"rgba(0, 0, 0, 0.5)","width":"160","roundRectRadius":"6"}},
+{"name":"EventCommandByCode","status":true,"description":"コードとパラメータを指定してイベントコマンドを実行します。","parameters":{}},
+{"name":"MVJoint","status":true,"description":"ツクールMZ上でMVのプラグインを無理やり動かします。","parameters":{}},
+{"name":"DTextPicture","status":true,"description":"動的文字列ピクチャ生成プラグイン","parameters":{"itemIconSwitchId":"0","lineSpacingVariableId":"0","frameWindowSkin":"","frameWindowPadding":"18","padCharacter":"0","prefixText":""}}
 ];
